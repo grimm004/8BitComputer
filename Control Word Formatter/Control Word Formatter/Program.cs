798124faf91d8fba83054cdc919689e6b6ea6946
rohan-inston <reggiedavies26@gmail.com>
2018-08-27 20:18:38 +0100

    Little formatter to remove x's from spreadsheet
    Tried doing this in excel and ripped my hair out. Had to do it in C#.

---
+++ Control Word Formatter/Control Word Formatter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApplication24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] readFile = File.ReadAllLines(@"\Documents\Control word text.txt");
+            List<string> output = new List<string>();
+            foreach (string line in readFile)
+            {
+                string[] addressWord = line.Split(',');
+                int count = 0;
+                foreach (char c in addressWord[0])
+                {
+                    if (c == 'x') count++;
+                }
+                for (int i = 0; i < Math.Pow(2, count); i++)
+                {
+                    string result = Convert.ToString(i, 2);
+                    int lenSave = result.Length;
+                    for (int j = 0; j < count - lenSave; j++)
+                    {
+                        result = "0" + result;
+                    }
+                    int charIndex = 0;
+                    string address = "";
+                    for (int j = 0; j < addressWord[0].Length; j++)
+                    {
+                        if (addressWord[0][j] == 'x')
+                        {
+                            address += result[charIndex];
+                            charIndex++;
+                        }
+                        else address += addressWord[0][j];
+                    }
+                    output.Add(address + "," + addressWord[1]);
+                }
+            }
+            File.WriteAllLines(@"Documents\Control word text II.txt", output.ToArray());
+        }
+    }
+}
